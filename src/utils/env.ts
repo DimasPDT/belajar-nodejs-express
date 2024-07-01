b22be8e690166bbbaf06cb085b0d511296f0781c
DimasPDT <dhimazpdt@gmail.com>
2024-07-01 21:04:42 +0700

    push add env

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,6 +1,6 @@
-export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY || "";
+export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY || "452985478462667";
 export const CLOUDINARY_API_SECRET: string =
-  process.env.CLOUDINARY_API_SECRET || "";
+  process.env.CLOUDINARY_API_SECRET || "0TyHrnVK_R8DXqgU9kRDSwYBrI4";
 export const CLOUDINARY_CLOUD_NAME: string =
-  process.env.CLOUDINARY_CLOUD_NAME || "";
+  process.env.CLOUDINARY_CLOUD_NAME || "dhxvopzik";
 export const DATABASE_URL: string = "mongodb+srv://dhimazpdt:b645DZSLcyla7ysI@belajar-backend.awfhlks.mongodb.net/?retryWrites=true&w=majority&appName=belajar-backend";
